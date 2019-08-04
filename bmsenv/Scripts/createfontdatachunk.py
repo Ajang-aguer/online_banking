87e93ef8fc5d08fdcec6717aa7fe421daabecf30
Ajang-aguer <mjmonyjang@gmail.com>
2019-08-03 21:10:12 -0600

    First Commit

---
+++ bmsenv/Scripts/createfontdatachunk.py
@@ -0,0 +1,16 @@
+#!c:\users\ajang\desktop\projects\python\django\online_banking\bmsenv\scripts\python.exe
+from __future__ import print_function
+import base64
+import os
+import sys
+
+if __name__ == "__main__":
+    # create font data chunk for embedding
+    font = "Tests/images/courB08"
+    print("    f._load_pilfont_data(")
+    print("         # %s" % os.path.basename(font))
+    print("         BytesIO(base64.decodestring(b'''")
+    base64.encode(open(font + ".pil", "rb"), sys.stdout)
+    print("''')), Image.open(BytesIO(base64.decodestring(b'''")
+    base64.encode(open(font + ".pbm", "rb"), sys.stdout)
+    print("'''))))")
---
+++ bmsenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ajang\desktop\projects\python\django\online_banking\bmsenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
